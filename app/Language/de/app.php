333a254f07b1b6ebf1ba80643c695753d36019f2
elektr0nisch <contact@elektronisch.dev>
2022-09-03 23:55:41 +0200

    Initial commit

---
+++ app/Language/de/app.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'name' => 'Portal',
+    'copyright' => '&copy; 2022 Freie Waldorfschule Augsburg<br><small>Entwickelt von <a href="https://elektronisch.dev" target="_blank">Linus Groschke</a></small>',
+    'error' => [
+        'exception' => [
+            'title' => 'Systemfehler!',
+            'text' => 'Es ist ein Fehler aufgetreten. Bitte wenden Sie sich an den technischen Support!'
+        ],
+        '404' => [
+            'title' => 'Nicht gefunden!',
+            'text' => 'Die von Ihnen angeforderte Seite konnte nicht gefunden werden.'
+        ]
+    ]
+];
---
+++ app/Views/errors/html/error_404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="<?= service('request')->getLocale(); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?= lang('app.name') ?></title>
+
+    <link href="<?= base_url('/') ?>/assets/img/logo.png" rel="icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+</head>
+
+<body>
+<div class="container px-4 mt-4">
+    <div class="row mt-5 justify-content-center">
+        <div class="col-lg-8 text-center">
+            <img src="<?= base_url('/') ?>/assets/img/logo.png" width="100" height="100" alt="" class="mx-auto d-block">
+            <h1 class="mb-5"><?= lang('app.name') ?></h1>
+            <hr>
+            <h4><?= lang('app.error.404.title') ?></h4>
+            <p><?= lang('app.error.404.text') ?><br/>
+                <a href="<?= base_url('/') ?>">Klicken Sie hier, um zur Startseite zu gelangen.</a>
+            </p>
+            <hr>
+        </div>
+    </div>
+</div>
+
+<div class="text-center mt-5">
+    <p class="text-muted"><?= lang('app.copyright') ?></p>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
