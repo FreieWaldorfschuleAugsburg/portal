52c88d0dcad08a665603f7718ce8fe88d3d1dfb4
elektr0nisch <contact@elektronisch.dev>
2023-04-20 01:06:41 +0200

    Add matomo tracking

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -10,6 +10,22 @@
 
     <link href="<?= base_url('/') ?>/assets/css/output.css" rel="stylesheet">
     <script src="<?= base_url() ?>/assets/script.js"defer></script>
+
+    <!-- Matomo -->
+    <script>
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//matomo.waldorf-augsburg.de/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
 </head>
 
 <body>
