fe1d24a3ef04f85fabaa9c4b9f9353ce68d44e8f
Linus Groschke <mail@linusgke.de>
2025-12-30 09:52:30 +0100

    Remove PasswordChangeView card header

--- app/Views/password/PasswordChangeView.php
+++ app/Views/password/PasswordChangeView.php
@@ -5,9 +5,6 @@
 
     <div class="col-lg-12">
         <div class="card mt-3 mb-3">
-            <div class="card-header">
-                <i class="fas fa-key"></i> <?= lang('password.change.cardHeadline') ?>
-            </div>
             <div class="card-body">
                 <?= form_open() ?>
                 <div class="input-group row mb-3">
