4d05e56b71641d3a112800b1649a0e15dcd8377e
Linus Groschke <mail@linusgke.de>
2025-12-30 09:53:35 +0100

    Remove ProfileView card header

--- app/Views/profile/ProfileView.php
+++ app/Views/profile/ProfileView.php
@@ -5,9 +5,6 @@
 
     <div class="col-lg-12">
         <div class="card mt-3 mb-3">
-            <div class="card-header">
-                <i class="fas fa-info-circle"></i> <?= lang('profile.cardHeadline') ?>
-            </div>
             <div class="card-body">
                 <?= form_open_multipart('profile') ?>
 
--- app/Views/studentReset/StudentResetView.php
+++ app/Views/studentReset/StudentResetView.php
@@ -5,9 +5,6 @@
 
     <div class="col-lg-12">
         <div class="card mt-3 mb-3">
-            <div class="card-header">
-                <i class="fas fa-user-graduate"></i> <?= lang('studentReset.cardHeadline') ?>
-            </div>
             <div class="card-body">
 
             </div>
