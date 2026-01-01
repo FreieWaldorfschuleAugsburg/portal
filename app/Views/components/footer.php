76c2faff563734f02dd0e5a2cf598de23e7af3df
Linus Groschke <mail@linusgke.de>
2026-01-01 14:37:43 +0100

    Automatically change year in footer copyright line

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,5 +1,5 @@
 <div class="text-center mt-3">
-    <p class="text-muted"><?= lang('app.copyright') ?></p>
+    <p class="text-muted"><?= sprintf(lang('app.copyright'), date("Y")) ?></p>
 </div>
 </div>
 <script src="<?= base_url('/') ?>/assets/js/popper.min.js"></script>
