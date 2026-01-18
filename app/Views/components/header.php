5063ef6f7ac328da600c5d34b7fa59a65a15ed89
Linus Groschke <mail@linusgke.de>
2026-01-18 22:56:31 +0100

    Check if user variable is set

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -32,7 +32,7 @@ use function App\Helpers\user;
             _paq.push(['setTrackerUrl', u + 'matomo.php']);
             _paq.push(['setSiteId', '3']);
 
-            <?php if ($user): ?>
+            <?php if (isset($user)): ?>
             _paq.push(['setUserId', '<?= $user->getUsername() ?>'])
             <?php endif; ?>
 
